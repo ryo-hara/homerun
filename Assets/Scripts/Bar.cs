b727276845fc0be8ec99afffdf32db0fa725bb8d
ryo <akstkr@gmail.com>
2023-08-04 00:39:40 +0900

    バーを置いて回転させる

---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Bar : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotate = new Vector3(-90, 0, 0);
+
+    [SerializeField]
+    private int rotateFrame = 60;
+    
+    private float ROTATE_INTERVAL = 1.0f;
+
+    private IDisposable disposable = null;
+    
+    void Start()
+    {
+        var nowRotate = transform.rotation.eulerAngles;
+        var rotateDiff = rotate - nowRotate;
+        var updateRotate = rotateDiff / rotateFrame;
+
+        var frame = 0;
+        disposable = this.UpdateAsObservable().Subscribe(_ =>
+        {
+            frame++;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + updateRotate);
+            if (frame >= rotateFrame)
+            {
+                disposable?.Dispose();
+            }
+        });
+    }
+}
