5cdbab7d0b1eba0f5e9253682d46118ff4b434a4
ryo <akstkr@gmail.com>
2023-08-05 03:23:50 +0900

    Prefab化したControlを順番に表示する

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,27 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private List<IGameControl> controlList = new List<IGameControl>();
+    [SerializeField]
+    private List<GameObject> controlList = new List<GameObject>();
 
+    private List<GameObject> instantiateList = new List<GameObject>();
+    private List<IGameControl> gameControlList = new List<IGameControl>();
     private void Awake()
     {
-        controlList.Add(new TitleControl());
-        controlList.Add(new GamePlayControl());
-        controlList.Add(new ResultControl());
+        foreach (var control in controlList)
+        {
+            var instantiateObject = Instantiate(control);
+            instantiateObject.SetActive(false);
+            gameControlList.Add(instantiateObject.GetComponent<IGameControl>());
+            instantiateList.Add(instantiateObject);
+        }
         StartCoroutine(ExecControl());
     }
     
     private IEnumerator ExecControl()
     {
-        foreach (var control in controlList)
+        for (int i = 0; i < instantiateList.Count; i++)
         {
-            yield return control.Execute();
+            instantiateList[i].SetActive(true);
+            yield return gameControlList[i].Execute();
+            instantiateList[i].SetActive(false);
         }
-
         yield return null;
     }
 }
